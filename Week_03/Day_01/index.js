9115fd027acd81cdcfdb7f67982afcdc913ce695
uhassantoor <uhassantoor@gmail.com>
2024-09-10 16:40:26 +0500

    JavaScript Day_01

---
+++ Week_03/Day_01/index.js
@@ -0,0 +1,18 @@
+
+
+var name = "Umer Hassan"
+
+var one = 22;
+var two = 11;
+var add = one + two ;
+var mul = one * two;
+var div = one/two;
+
+document.write("Sum is : ", add  );
+document.write("Mul is : " , mul )
+
+console.log("Divison is: ", div)
+
+// alert(('Umer'))
+
+
