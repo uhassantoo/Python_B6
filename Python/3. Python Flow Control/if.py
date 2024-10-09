d4c9099e7e446b888b345fcfd248d54f69792e95
uhassantoor <uhassantoor@gmail.com>
2024-10-09 10:22:15 +0500

    Python Flow Control

---
+++ Python/3. Python Flow Control/if.py
@@ -0,0 +1,24 @@
+number = int(input('Enter a number :'))
+# check if number is greater than 0
+if number > 0:
+    print(f'{number} is a postive number')
+    
+print("This statement is outside of if statment")
+
+
+# Indentation in Python
+x =2 
+total = 0
+# if statement
+if x != 0:
+    total += x
+    print(total)
+    #end if statement
+    
+# if else statement
+num = int(input('Enter a number :'))
+if num >=0:
+    print('Positive Number')
+else:
+    print('Not a Positive Number')
+    
