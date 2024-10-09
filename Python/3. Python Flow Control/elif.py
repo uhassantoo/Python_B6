c27ab372d309f308ff85b6131906eac4019bdff9
uhassantoor <uhassantoor@gmail.com>
2024-10-09 15:25:58 +0500

    Python Flow Control

---
+++ Python/3. Python Flow Control/elif.py
@@ -0,0 +1,28 @@
+# if condition:
+#     #code
+# elif condition2:
+#     #code
+
+# else:
+#     #code 
+
+num = -18
+if num>0 :
+    print('Postive Number')
+elif num<0:
+    print("Negative Number")
+else:
+    print('Zero')
+    
+# Python Nested If 
+number = -10
+
+if number > 0:
+    #inner 
+     if number == 0:
+         print('Number is 0')
+     else:
+         print('Number is Positive')
+else:
+    print('Number is Negative')
+        
---
+++ Python/3. Python Flow Control/loops.py
@@ -0,0 +1,44 @@
+lang = ['PHP', 'PYTHON ','C++']
+for languages in lang:
+      print(languages)
+      
+name = 'Umer'
+for a in name:
+    print(a)
+
+for i in range(0,12,2):
+    print(i)
+    
+values = range(6)
+for i in range(6):
+    print(i)
+
+
+print('While Loop')
+number = 1
+
+while number <=5:
+    print(number)
+    number = number + 1
+    
+print('Another while loop')
+
+nums = int(input('Enter a number:'))
+while nums !=0:
+    print(f'You entered: {nums}')
+    nums = int(input('Enter a number:: '))
+    
+print('The end')
+
+print('Inifinte')
+#infinite while loop
+age = 20 
+
+# while age>18:
+#     print('You Save')
+
+age = 32
+while True:
+    print('yOU SAVE')
+   
+    
