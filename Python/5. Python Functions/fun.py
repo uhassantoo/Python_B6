1dcd7d8dab07d3467cdbe6df3d28a6bf3bdade5b
uhassantoor <uhassantoor@gmail.com>
2024-10-10 16:31:41 +0500

    Python Functions

---
+++ Python/5. Python Functions/fun.py
@@ -0,0 +1,36 @@
+# create a function
+
+def greet():
+    print('Hellow Python Class')
+  
+    
+#call a function
+
+greet()
+
+# Python arguments
+
+def greet(name):
+    print('hellow :',name)
+
+
+greet('Umer')
+
+def greet(name,age):
+    print("hellow:",name,age)
+
+greet('umer',29)
+
+# function add to number\
+    
+def add(num1,num2):
+    sum = num1 + num2
+    print('Sum is :', sum)
+    
+    
+#call a function
+add(5,4)
+
+#def cal
+# def cal(num1,num2):
+#     mul = num1* num2
