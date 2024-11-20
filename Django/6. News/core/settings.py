21efa0c0905461f502cb4eb64064ed0fc06964a4
uhassantoor <uhassantoor@gmail.com>
2024-11-20 15:07:07 +0500

    Newspaper App

--- Django/6. News/core/settings.py
+++ Django/6. News/core/settings.py
@@ -133,3 +133,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
